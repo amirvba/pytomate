fd970f515b9d325588f248228889ed95de59c50b
amir <amir.meshkati@gmail.com>
2018-11-17 15:27:14 +0100

    Update Frequently_Used_Functions.py

--- Frequently_Used_Functions.py
+++ Frequently_Used_Functions.py
@@ -1,3 +1,5 @@
+import os
+
 def get_filenames_with_ending(str_file_address, str_ending):
     
     assert type(str_ending)==str , "The ending must be string!"
@@ -9,7 +11,6 @@ def get_filenames_with_ending(str_file_address, str_ending):
     
     for str_item in lst_filenames:
         if str_item[:len(str_ending)]==str_ending:
-            print(str_item)
             lst_return.append(str_item)
             
     return lst_return
